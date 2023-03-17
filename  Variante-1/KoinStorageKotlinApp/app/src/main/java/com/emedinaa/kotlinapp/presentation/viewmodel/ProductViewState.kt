768a5eceb09c07688970af923665b6f9fb8279ce
Fahed Hermoza <fhermoza@emeal.nttdata.com>
2023-03-17 17:04:33 -0500

    Removed project

---  Variante-1/KoinStorageKotlinApp/app/src/main/java/com/emedinaa/kotlinapp/presentation/viewmodel/ProductViewState.kt
+++
@@ -1,8 +0,0 @@
-package com.emedinaa.kotlinapp.presentation.viewmodel
-
-import com.emedinaa.kotlinapp.domain.model.Product
-
-data class ProductViewState(
-    val loading: Boolean = true,
-    val products: List<Product> = emptyList(),
-)

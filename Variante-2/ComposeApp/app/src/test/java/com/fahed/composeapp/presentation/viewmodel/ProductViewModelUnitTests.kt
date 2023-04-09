a3c3315b86a783b78c8e231b6c489a68067438af
Fahed Hermoza <fhermoza@emeal.nttdata.com>
2023-04-09 17:08:47 -0500

    Added unit test for initLoadProduct() - Variant2

--- Variante-2/ComposeApp/app/src/test/java/com/fahed/composeapp/presentation/viewmodel/ProductViewModelUnitTests.kt
+++ Variante-2/ComposeApp/app/src/test/java/com/fahed/composeapp/presentation/viewmodel/ProductViewModelUnitTests.kt
@@ -1,6 +1,8 @@
 package com.fahed.composeapp.presentation.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.fahed.composeapp.core.utils.getOrAwaitValue
+import com.fahed.composeapp.domain.ProductRepository
 import com.fahed.composeapp.domain.usecase.AddProductUseCase
 import com.fahed.composeapp.domain.usecase.ClearProductUseCase
 import com.fahed.composeapp.domain.usecase.FetchProductUseCase
@@ -9,12 +11,14 @@ import com.fahed.composeapp.domain.usecase.UpdateProductUseCase
 import com.fahed.composeapp.domain.usecase.mock.RepositoryMock
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,6 +27,7 @@ import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import kotlin.time.ExperimentalTime
 
 /***
@@ -38,7 +43,7 @@ class ProductViewModelUnitTests {
     val instantExecutorRule = InstantTaskExecutorRule()
     private val testDispatcher = StandardTestDispatcher()
 
-    private val fetchProductUseCase = Mockito.mock(FetchProductUseCase::class.java)
+    private var fetchProductUseCase = Mockito.mock(FetchProductUseCase::class.java)
     private val clearProductUseCase = Mockito.mock(ClearProductUseCase::class.java)
     private val addProductUseCase = Mockito.mock(AddProductUseCase::class.java)
     private val updateProductUseCase = Mockito.mock(UpdateProductUseCase::class.java)
@@ -66,10 +71,20 @@ class ProductViewModelUnitTests {
         Dispatchers.resetMain()
     }
 
-    //https://developer.android.com/kotlin/flow/test?hl=es-419
-    // Will be feature in the future
     @Test
-    fun `init loadProduct with list`()= runTest {}
+    fun `check initLoadProduct with some element`()= runTest {
+        //given
+        val expectedProducts = flowOf(RepositoryMock.listProduct())
+        val productRepository = Mockito.mock(ProductRepository::class.java)
+        whenever(productRepository.getAllProducts()).thenReturn(expectedProducts)
+        fetchProductUseCase = FetchProductUseCase(productRepository)
+        //when
+        buildViewModel()
+        productViewModel.loadProducts()
+        val onProduct = productViewModel.onProducts.getOrAwaitValue()
+        //then
+        Assert.assertEquals(onProduct, RepositoryMock.listProduct())
+    }
 
     @Test
     fun `check addNewProduct is add product`()= runTest {
